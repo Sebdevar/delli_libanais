0e11be5e2b5b4e0c6a2ab49602b53a1fe62bddd1
Sebastien De Varennes <sebdevar@gmail.com>
2021-02-10 18:18:22 -0500

    Add GooglePlaces.context.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import GooglePlacesProvider from './contexts/GooglePlaces.context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GooglePlacesProvider>
+      <App />
+    </GooglePlacesProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
