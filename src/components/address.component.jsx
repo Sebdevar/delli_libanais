96b355134fcb2d8745c73d126bc668753e884534
Kikiwako <vingi20@hotmail.com>
2021-02-10 18:21:33 -0500

    making some delivery links

---
+++ src/components/address.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// const googleMapLink = 'https://goo.gl/maps/6qSA6VKazErhkVPB6';
+
+const Address = () => (
+  <StyledIframe
+    title="carte"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43670.13009501311!2d-71.29262538617927!3d46.836147265189204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb897e95023b1df%3A0x70494d83c60ed07b!2sRestaurant%20libanais%20Lebourgneuf!5e0!3m2!1sen!2sca!4v1612990272867!5m2!1sen!2sca"
+    width="240"
+    height="240"
+    frameBorder="0"
+    allowFullScreen=""
+    aria-hidden="false"
+    tabIndex="0"
+  />
+);
+
+const StyledIframe = styled.iframe`
+  border:0;
+`;
+
+export default Address;
--- src/components/banner.component.jsx
+++ src/components/banner.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import bannerImage from '../images/Banner.webp';
+
 const Banner = () => (
-  <StyledBanner>test</StyledBanner>
+  <StyledBanner>
+    <StyledImage src={bannerImage} alt="Banière" />
+    1170 Boulevard Lebourgneuf, Québec, QC G2K 2E3
+  </StyledBanner>
 );
 
+const StyledImage = styled.img`
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  display:block;
+`;
+
 const StyledBanner = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  grid-column-start: 1;
+  grid-column-end: 6;
+
+  color:white;
+  font-weight: bold;
 `;
 export default Banner;
