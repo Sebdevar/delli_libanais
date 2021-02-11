ca4e95933380b2b5c3125c884628b48ab600462e
Kikiwako <vingi20@hotmail.com>
2021-02-11 09:01:26 -0500

    Changed address font and styling

--- src/components/banner.component.jsx
+++ src/components/banner.component.jsx
@@ -6,12 +6,25 @@ import bannerImage from '../images/Banner.webp';
 const Banner = () => (
   <StyledBanner>
     <StyledImage src={bannerImage} alt="Banière" />
-    1170 Boulevard Lebourgneuf, Québec, QC G2K 2E3
+    <Text>
+      1170 Boulevard Lebourgneuf, Québec, QC G2K 2E3
+    </Text>
   </StyledBanner>
 );
 
+const Text = styled.div`
+  position: relative;
+  /* top: 50%; */
+  left: 50%;
+  transform: translate(-50%, -90%);
+  text-shadow: 4px 4px 3.5px rgba(0,0,0,0.5);
+
+  font-family: 'Lateef', cursive;
+  font-size: 25pt;
+`;
+
 const StyledImage = styled.img`
-  width: 90%;
+  width: 100%;
   margin-left: auto;
   margin-right: auto;
   display:block;
