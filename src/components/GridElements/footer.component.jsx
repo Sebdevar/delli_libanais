f823acfe2efb7534c71fc84d37366b58a4935adf
Kikiwako <vingi20@hotmail.com>
2021-02-15 22:26:39 -0500

    Got Nice Feet

--- src/components/GridElements/footer.component.jsx
+++ src/components/GridElements/footer.component.jsx
@@ -3,22 +3,29 @@ import styled from 'styled-components';
 
 const Footer = () => (
   <FooterContainer>
-    ©
-    {' '}
     <a href="https://github.com/Sebdevar">Sébastien De Varennes</a>
-    {' '}
-    &
-    {' '}
+    {' & '}
     <a href="https://github.com/kikiwako">Vincent Girard</a>
+    {' © 2021'}
   </FooterContainer>
 );
 
 const FooterContainer = styled.div`
+  display: flex;
   grid-area: footer;
   color: white;
   background-color: #15386f;
-  text-align: left;
-  padding-left: 3em;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  white-space: pre-wrap;
+  background-image:
+    linear-gradient(
+      to right,
+      #15386f,
+      #154694,
+      #15386f
+    );
 
   a {
     color: #f1f1f1;
