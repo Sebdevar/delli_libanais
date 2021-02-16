043aa4c381a97ff1e00e5c0b083aa64f3d8ae103
Kikiwako <vingi20@hotmail.com>
2021-02-15 19:58:29 -0500

    randomized links

--- src/components/GridElements/deliveryLinks.components.jsx
+++ src/components/GridElements/deliveryLinks.components.jsx
@@ -5,12 +5,29 @@ import DoordashLink from '../Links/doordashLink.component';
 import UberEatsLink from '../Links/uberEatsLink.component';
 import SkipLink from '../Links/skipLink.component';
 
+function shuffle(array) {
+  const output = array;
+
+  for (let i = output.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [output[i], output[j]] = [output[j], output[i]];
+  }
+
+  return output;
+}
+
+const shuffledLinks = shuffle([
+  (<DoordashLink />),
+  (<UberEatsLink />),
+  (<SkipLink />),
+]);
+
 const deliveryLinks = () => (
   <DeliveryLinks>
     <StyledGrid>
-      <DoordashLink />
-      <UberEatsLink />
-      <SkipLink />
+      {shuffledLinks[0]}
+      {shuffledLinks[1]}
+      {shuffledLinks[2]}
     </StyledGrid>
   </DeliveryLinks>
 );
