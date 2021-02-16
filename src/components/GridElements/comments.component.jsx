3a045784db453acba83da6159c789842da61ef95
Sebastien De Varennes <sebdevar@gmail.com>
2021-02-15 20:10:18 -0500

    Fix comments carousel

--- src/components/GridElements/comments.component.jsx
+++ src/components/GridElements/comments.component.jsx
@@ -1,13 +1,12 @@
-import React, { useMemo, useReducer } from 'react';
+import React, {
+  useEffect, useMemo, useReducer, useState,
+} from 'react';
 import styled from 'styled-components';
 import { useGoogleInfos } from '../../contexts/googleInfos.context';
 import CommentsReducer, { useActions } from './comments.reducer';
 
 const initialState = {
   reviews: [],
-  initialX: null,
-  currentX: null,
-  offsetX: 0,
 };
 
 const CommentsCarousel = () => {
@@ -16,10 +15,19 @@ const CommentsCarousel = () => {
     CommentsReducer,
     {
       ...initialState,
-      reviews: googleInfos.reviews.concat(googleInfos.reviews),
+      reviews: googleInfos.reviews,
     },
   );
   const actions = useActions(dispatch);
+  const [animation, setAnimation] = useState('next');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAnimation('next');
+      actions.nextReview();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [actions]);
 
   const carouselContent = useMemo(() => state.reviews.map((review) => (
     <CommentBox>
@@ -28,63 +36,116 @@ const CommentsCarousel = () => {
     </CommentBox>
   )), [state.reviews]);
 
-  const startSwipe = (event) => {
-    actions.startSwipe(event);
+  const handlePreviousClick = () => {
+    setAnimation('previous');
+    actions.previousReview();
   };
 
-  const updateCursorPosition = (event) => {
-    if (state.initialX !== null) {
-      actions.move(event);
-    }
-  };
-
-  const endSwipe = (event) => {
-    event.preventDefault();
-    if (state.offsetX >= 360) {
-      actions.previousReview();
-    }
-    if (state.offsetX <= -360) {
-      actions.nextReview();
-    }
-    actions.endSwipe(event);
+  const handleNextClick = () => {
+    setAnimation('next');
+    actions.nextReview();
   };
 
   return (
-    <CarouselContainer
-      onMouseDown={startSwipe}
-      onMouseMove={updateCursorPosition}
-      onMouseUp={endSwipe}
-      onMouseLeave={endSwipe}
-      onTouchStart={startSwipe}
-      onTouchMove={updateCursorPosition}
-      onTouchEnd={endSwipe}
-      onTouchCancel={endSwipe}
-    >
-      <CarouselContent offset={state.offsetX} itemcount={state.reviews.length}>
+    <CarouselContainer>
+      <PreviousButton onClick={handlePreviousClick}>
+        <LeftArrow />
+      </PreviousButton>
+      <CarouselContent
+        animation={animation}
+        key={state.reviews[0].time}
+      >
         {carouselContent}
       </CarouselContent>
+      <Next onClick={handleNextClick}>
+        <RightArrow />
+      </Next>
     </CarouselContainer>
   );
 };
 
 const CarouselContainer = styled.div`
   grid-area: comments;
+  position: relative;
   width: 100%;
   height: 100%;
   overflow: hidden;
   font-size: 16px;
 `;
 
+const PreviousButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 3em;
+  height: 15em;
+  background-color: rgba(100, 100, 100, 0.25);
+  z-index: 1;
+
+  &:hover {
+    background-color: rgba(100, 100, 100, 0.5);
+    transform: scale(1.25);
+    transition: 0.2s all ease-out;
+  }
+`;
+
+const LeftArrow = styled.div`
+  width: 0;
+  height: 0;
+  border-top: 2em solid transparent;
+  border-bottom: 2em solid transparent;
+  border-right: 1.5em solid black;
+`;
+
+const Next = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 3em;
+  height: 15em;
+  background-color: rgba(100, 100, 100, 0.25);
+  z-index: 1;
+
+  &:hover {
+    background-color: rgba(100, 100, 100, 0.5);
+    transform: scale(1.25);
+    transition: 0.2s all ease-out;
+  }
+`;
+
+const RightArrow = styled.div`
+  width: 0;
+  height: 0;
+  border-top: 2em solid transparent;
+  border-bottom: 2em solid transparent;
+  border-left: 1.5em solid black;
+`;
+
 const CarouselContent = styled.div.attrs((props) => ({
   style: {
-    left: `${(props.offset / 16) - (props.itemcount * 22.5) + 22.5}em`,
+    animation: `${props.animation} ease-in-out 1.5s`,
   },
 }))`
   display: flex;
+  left: -67.5em;
   flex-wrap: nowrap;
   align-items: center;
   position: relative;
   height: 100%;
+
+  @keyframes next {
+    from { transform: translateX(45em); }
+    to { transform: translateX(none); }
+  }
+
+  @keyframes previous {
+    from { transform: translateX(-45em); }
+    to { transform: translateX(none); }
+  }
 `;
 
 const CommentBox = styled.div`
